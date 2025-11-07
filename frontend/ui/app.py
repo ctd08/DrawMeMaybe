8e81befcc50ef34053ae59a537f504cb81eb115a
ctd08 <cristina.tutunariu@yahoo.com>
2025-11-07 15:04:32 +0100

    added camera page with frame and buttons (not functional)

--- frontend/ui/app.py
+++ frontend/ui/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from ui_screensaver import show_screensaver
 from ui_consent import show_consent_form
+from ui_camera import show_camera_stage
 from ui_chat import show_chat_ui
 from utils.styles import inject_global_css
 
@@ -33,11 +34,15 @@ if "touched" not in st.session_state:
 if "consent_accepted" not in st.session_state:
     # set to True to skip consent while developing
     st.session_state.consent_accepted = False
+if "photo_captured" not in st.session_state:     # ⬅️ NEW
+    st.session_state.photo_captured = False
 
 # 👉 flow
 if not st.session_state.touched:
     show_screensaver()
 elif not st.session_state.consent_accepted:
     show_consent_form()
+elif not st.session_state.photo_captured:        # ⬅️ NEW stage
+    show_camera_stage()
 else:
     show_chat_ui()
