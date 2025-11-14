1661e3f76a720ff87b62dc37653dcc7881d60303
ctd08 <cristina.tutunariu@yahoo.com>
2025-11-14 19:46:28 +0100

    created new files and set the router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import ScreensaverView from "./views/ScreensaverView.vue";
+import ConsentView from "./views/ConsentView.vue";
+import CameraView from "./views/CameraView.vue";
+import ChatView from "./views/ChatView.vue";
+
+const routes = [
+  { path: "/", name: "screensaver", component: ScreensaverView },
+  { path: "/consent", name: "consent", component: ConsentView },
+  { path: "/camera", name: "camera", component: CameraView },
+  { path: "/chat", name: "chat", component: ChatView },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
