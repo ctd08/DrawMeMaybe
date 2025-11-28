d6e8a79843b3077df8419dba8ca3662ea617dc94
cristina <cristina.tutunariu@tha.de>
2025-11-28 19:02:53 +0100

    funktionierendes modell

--- src/testens.py
+++ src/testens.py
@@ -3,28 +3,26 @@ from diffusers import StableDiffusionImg2ImgPipeline
 from PIL import Image
 import random
 
-# Bild laden
-init_image = Image.open("src/assets/tüp.png").convert("RGB")
+# Inputbild 
+init_image = Image.open("src/assets/whiteguy2.png").convert("RGB")
+init_image = init_image.resize((512, 512))
 
-# Device wählen
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Pipeline laden
+# Pipeline laden 
 pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
-    "timbrooks/toonify", 
-    dtype=torch.float16,
-#    use_auth_token=True  # falls private Modelle
+    "runwayml/stable-diffusion-v1-5",  
 ).to(device)
 
+# Prompt definieren 
 prompt = (
-    "clean caricature portrait, stylized face, soft shading, strong outlines, "
-    "exaggerated proportions, high quality"
+    "funny caricature, huge head, tiny body, exaggerated features, cartoon style, bold outlines, simple shapes"
+
 )
 
-# Stärke: Wie stark das Original verändert wird
-strength = 0.25
-
-# Seed: Für Zufall
+# img2img Parameter
+strength = 0.7           # wie stark das Original verändert wird
+guidance_scale = 7.5      # wie stark das Modell dem Prompt folgt
 seed = random.randint(0, 999999999)
 generator = torch.Generator(device).manual_seed(seed)
 
@@ -33,9 +31,14 @@ result = pipe(
     prompt=prompt,
     image=init_image,
     strength=strength,
-    guidance_scale=2.0,     # bei SDXL turbo klein halten
+    guidance_scale=guidance_scale,
     generator=generator
 ).images[0]
 
-# Speichern
-result.save("src/assets/caricature.png")
+# Speichern und anzeigen
+result.save("src/assets/caricature_result.png")
+
+
+
+
+
